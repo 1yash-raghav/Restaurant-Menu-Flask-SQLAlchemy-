efaa8ca27028f43c33e43647a47d5e24fd123971
Yash Raghav <1yashraghav@gmail.com>
2020-10-29 19:07:58 +0530

    Backend fully ready with CRUD

--- Server.py
+++ Server.py
@@ -17,7 +17,6 @@ def restaurants():
     restaurants = session.query(Restaurant).all()
     return render_template('restaurants.html', restaurants=restaurants)
 
-
 @app.route('/restaurants/new', methods={'GET', 'POST'})
 def addNewRestaurant():
     if(request.method == "POST"):
@@ -28,6 +27,18 @@ def addNewRestaurant():
         return redirect(url_for("restaurants"))
     else:
         return render_template('NewRestaurant.html')
+
+
+@app.route('/restaurants/<int:restaurant_id>/delete', methods={'GET', 'POST'})
+def DeleteRestaurant(restaurant_id):
+    itemToDelete = session.query(Restaurant).filter_by(
+        id=restaurant_id).one()
+    if(request.method == 'POST'):
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for("restaurants"))
+    else:
+        return render_template('deleteItem.html')
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
@@ -63,7 +74,14 @@ def EditItem(restaurant_id, menu_id):
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete', methods={'GET', 'POST'})
 def DeleteItem(restaurant_id, menu_id):
-    return ("Page to delete")
+    itemToDelete = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).filter_by(id=menu_id).one()
+    if(request.method == 'POST'):
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for("restaurantMenu", restaurant_id=restaurant_id))
+    else:
+        return render_template('deleteItem.html')
+
 
 
 if __name__ == '__main__':
